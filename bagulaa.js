640c3609f99d31be6649ede850da58dbbac3b0ad
diegosanchez <diego.sanchez@mercadolibre.com>
2017-11-02 20:29:20 -0300

    otro comentario mas

--- bagulaa.js
+++ bagulaa.js
@@ -2,6 +2,9 @@ function Bagulaa( bornPlace) {
     this.where = bornPlace;
 };
 
+/***
+ * otro comentario mas
+ * */
 Bagulaa.prototype.bumpsInto = function bumpsInto( anCharacterOrObject ) {
     return anCharacterOrObject.bumpsIntoBagulaa(this);
 };
